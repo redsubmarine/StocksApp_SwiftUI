95a119144033f11f1a50ef313bb2133eafb67a70
red <wonseok@duck.com>
2023-02-22 22:03:35 +0900

    add stocks api from SPM.

--- StocksApp/StocksAppApp.swift
+++ StocksApp/StocksAppApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import StocksAPI
 
 @main
 struct StocksAppApp: App {
+    let stocksAPI = StocksAPI()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    Task {
+                        do {
+                            let quotes = try await stocksAPI.fetchQuotes(symbols: "AAPL")
+                            print("--", quotes)
+                        } catch {
+                            print(error.localizedDescription)
+                        }
+                    }
+                }
         }
     }
 }
