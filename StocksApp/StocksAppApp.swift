b351d0e078b3e5089c34e00ff9985192df2caa7c
red <wonseok@duck.com>
2023-02-22 21:59:18 +0900

    Initial Commit

---
+++ StocksApp/StocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksAppApp.swift
+//  StocksApp
+//
+//  Created by 레드 on 2023/02/22.
+//
+
+import SwiftUI
+
+@main
+struct StocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
